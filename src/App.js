ee06d9d3c3c6d3390a88874fcd38c773ef897037
Giuseppe Gigliotti <giuseppegigliottidev@gmail.com>
2021-03-05 20:31:35 +0100

    major update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import reactDOM from 'react-dom';
-import './App.scss';
-import Logo from '../public/media/img/reactjs-fill.svg';
+import React from 'react'
+import reactDOM from 'react-dom'
+import './App.scss'
+import Logo from './assets/img/reactjs-fill.svg'
 
 const App = () => {
   return (
-    <div className="main">
-      <img className="logo" src={Logo} />
-      <h2 className="title">Hello World!</h2>
-      <p className="subtitle">This is a ReactJS boilerplate</p>
+    <div className='main'>
+      <img className='logo' src={Logo} />
+      <h2 className='title'>Hello World!</h2>
+      <p className='subtitle'>This is a ReactJS boilerplate</p>
     </div>
   )
 }
